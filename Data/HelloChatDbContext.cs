7ad66c1f52500612bcbc169eacc28049e8f3cd9f
david20033 <daviddimitrov203@gmail.com>
2025-03-04 14:52:40 +0200

    Implement DbContext with Identity support

---
+++ Data/HelloChatDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HelloChat.Data;
+
+public class HelloChatDbContext : IdentityDbContext<IdentityUser>
+{
+    public HelloChatDbContext(DbContextOptions<HelloChatDbContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,18 @@
+
 using HelloChat.Hubs;
+using Microsoft.EntityFrameworkCore;
+using HelloChat.Data;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnectionString");
+
+builder.Services.AddDbContext<HelloChatDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<HelloChatDbContext>();
 
 // Add services to the container.
+//builder.Services.AddDbContext<HelloChatDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
 builder.Services.AddControllersWithViews();
 builder.Services.AddSignalR();
 
@@ -22,10 +32,10 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
-app.MapHub<ChatHub>("/chatHub");
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
 
 app.Run();
