484ea12eb158839a310ea932a2b463734d540ff9
david20033 <daviddimitrov203@gmail.com>
2025-03-20 13:13:04 +0200

    Refactor java script code to work with new main page desing

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -14,53 +14,47 @@ connection.on("ReceiveMessage", function (messageId, message) {
     if (messageEl) {
         messageEl.remove();
     }
-    var messageContainer = document.createElement("div");
 
     var div = document.createElement("div");
     div.classList.add("message-row")
     div.classList.add("to")
-    var p = document.createElement("p");
-    p.classList.add("message-balon");
-    p.classList.add("message-to");
+    div.classList.add("left")
+    var messageDiv = document.createElement("div");
+    messageDiv.classList.add("message");
+    messageDiv.classList.add("received");
 
-    p.textContent = message;
-    p.id = messageId;
+    messageDiv.textContent = message;
+    messageDiv.id = messageId;
     const reactionContainer = createReactionContainer();
-    const { deleteSvg, reactSvg } = createMessageOptionsIcons();
-    div.appendChild(reactionContainer);
-    div.appendChild(deleteSvg);
-    div.appendChild(reactSvg);
-    messageContainer.appendChild(p);
-    messageContainer.style.order="0"
-    div.appendChild(messageContainer);
+    const { divDelete, divReact } = createMessageOptionsIcons("deleteSenderMessage", "3");
+    messageDiv.appendChild(reactionContainer);
+    div.appendChild(messageDiv);
+    div.appendChild(divDelete);
+    div.appendChild(divReact);
     document.getElementById("messages").appendChild(div);
-    var messagesDiv = document.getElementById("messages");
-    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    var container = document.getElementById("messages");
+    container.scrollTop = container.scrollHeight;
 });
 connection.on("SendMessage", function (messageId, message) {
-    var messageContainer = document.createElement("div");
-
     var div = document.createElement("div");
     div.classList.add("message-row")
     div.classList.add("from")
-    var p = document.createElement("p");
-    p.classList.add("message-balon");
-    p.classList.add("message-from");
+    div.classList.add("right")
+    var messageDiv = document.createElement("div");
+    messageDiv.classList.add("message");
+    messageDiv.classList.add("sent");
 
-    p.textContent = message;
-    p.id = messageId;
+    messageDiv.textContent = message;
+    messageDiv.id = messageId;
     const reactionContainer = createReactionContainer();
-    const { deleteSvg, reactSvg } = createMessageOptionsIcons();
-    div.appendChild(reactionContainer);
-    div.appendChild(deleteSvg);
-    div.appendChild(reactSvg);
-    messageContainer.appendChild(p);
-    messageContainer.style.order = "1"
-    div.appendChild(messageContainer);
+    const { divDelete, divReact } = createMessageOptionsIcons("deleteOwnMessage", "1");
+    messageDiv.appendChild(reactionContainer);
+    div.appendChild(messageDiv);
+    div.appendChild(divDelete);
+    div.appendChild(divReact);
     document.getElementById("messages").appendChild(div);
-
-    var messagesDiv = document.getElementById("messages");
-    messagesDiv.scrollTop = messagesDiv.scrollHeight;
+    var container = document.getElementById("messages");
+    container.scrollTop = container.scrollHeight;
 });
 connection.on("ReceiveTyping", function () {
     if (document.getElementById("TypingBalon") != null) {
@@ -110,14 +104,14 @@ connection.on("FriendConnected", function (userId) {
     userDiv.style.display = "inline";
 });
 connection.on("ReceiveSeen", function (messageId) {
-    const elements = document.querySelectorAll(".seen-time");
+    const elements = document.querySelectorAll(".read-status-message");
     const lastElement = elements[elements.length - 1];
     if (lastElement) {
         lastElement.remove();
     }
     var message = document.getElementById(messageId);
     var small = document.createElement("small");
-    small.classList.add("seen-time");
+    small.classList.add("read-status-message");
     var strong = document.createElement("strong");
     strong.innerText = "Read";
     small.appendChild(strong);
@@ -126,7 +120,7 @@ connection.on("ReceiveSeen", function (messageId) {
     const minutes = now.getMinutes().toString().padStart(2, '0');
     const currentTime = `${hours}:${minutes}`;
     small.appendChild(document.createTextNode(" "+currentTime));
-    message.appendChild(small);
+    message.parentElement.appendChild(small);
     var messagesDiv = document.getElementById("messages");
     messagesDiv.scrollTop = messagesDiv.scrollHeight;
 });
@@ -152,7 +146,7 @@ connection.on("ReceiveMessageReaction", function (MessageId, Reaction, UserId) {
     if (!messageReaction) {
         var div = document.createElement("div");
         div.classList.add("message-reaction");
-        if (messageBallon.classList.contains('message-to')) {
+        if (messageBallon.classList.contains('received')) {
             div.classList.add("left");
         } else { 
             div.classList.add("right");
@@ -251,13 +245,12 @@ document.getElementById("sendButton").addEventListener("click", function (event)
 });
 document.getElementById("messages").addEventListener("click", function (event) {
     const deleteBtn = event.target.closest(".message-options.deleteOwnMessage");
-
     if (deleteBtn) {
         const messageRow = deleteBtn.closest(".message-row");
         if (messageRow) {
-            const messageBalon = messageRow.querySelector(".message-balon");
-            if (messageBalon) {
-                const MessageId = messageBalon.id;
+            const message = messageRow.querySelector(".message");
+            if (message) {
+                const MessageId = message.id;
                 const ToId = document.getElementById("ToId").value;
                 const FromId = document.getElementById("FromId").value;
 
@@ -273,9 +266,9 @@ document.querySelectorAll(".message-row").forEach(function (element) {
     const deleteBtn = element.querySelector(".message-options.deleteSenderMessage");
     if (deleteBtn) {
         deleteBtn.addEventListener("click", function () {
-            const messageBalon = element.querySelector(".message-balon");
-            if (messageBalon) {
-                const MessageId = messageBalon.id;
+            const message = element.querySelector(".message");
+            if (message) {
+                const MessageId = message.id;
                 var FromId = document.getElementById("FromId").value;
                 connection.invoke("SendLocalDeleteMessage", FromId, MessageId).catch(function (err) {
                     console.error(err.toString());
@@ -348,14 +341,12 @@ document.addEventListener('DOMContentLoaded', function () {
                     e.target.classList.contains('laugh-reaction') ? 'Laugh' :
                         e.target.classList.contains('smile-reaction') ? 'Smile' :
                             e.target.classList.contains('angry-reaction') ? 'Angry' : null;
-
             if (reactionType) {
                 const ToId = document.getElementById("ToId").value;
                 const FromId = document.getElementById("FromId").value;
                 const parent = e.target.closest(".message-row"); 
-                const messageBalon = parent.querySelector(".message-balon");
-                const messageId = messageBalon.id;
-
+                const message = parent.querySelector(".message");
+                const messageId = message.id;
                 connection.invoke("SendMessageReaction", messageId, reactionType, FromId, ToId)
                     .catch(function (err) {
                         console.error(err.toString());
@@ -370,36 +361,19 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 
-function createMessageOptionsIcons() {
-    const deleteSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    deleteSvg.setAttribute("class", "message-options deleteOwnMessage size-6");
-    deleteSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    deleteSvg.setAttribute("fill", "none");
-    deleteSvg.setAttribute("viewBox", "0 0 24 24");
-    deleteSvg.setAttribute("stroke-width", "1.5");
-    deleteSvg.setAttribute("stroke", "currentColor");
+function createMessageOptionsIcons(DeleteClass,order) {
+    var divDelete = document.createElement("div");
+    divDelete.classList.add("message-options");
+    divDelete.classList.add(DeleteClass);
+    divDelete.style.order = order;
+    divDelete.innerText = "🗑️";
 
-    const deletePath = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    deletePath.setAttribute("stroke-linecap", "round");
-    deletePath.setAttribute("stroke-linejoin", "round");
-    deletePath.setAttribute("d", "m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0");
-    deleteSvg.appendChild(deletePath);
-
-    const reactSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    reactSvg.setAttribute("class", "message-options React size-6");
-    reactSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
-    reactSvg.setAttribute("fill", "none");
-    reactSvg.setAttribute("viewBox", "0 0 24 24");
-    reactSvg.setAttribute("stroke-width", "1.5");
-    reactSvg.setAttribute("stroke", "currentColor");
-
-    const reactPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
-    reactPath.setAttribute("stroke-linecap", "round");
-    reactPath.setAttribute("stroke-linejoin", "round");
-    reactPath.setAttribute("d", "M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z");
-    reactSvg.appendChild(reactPath);
-
-    return { deleteSvg, reactSvg };
+    var divReact = document.createElement("div");
+    divReact.classList.add("message-options");
+    divReact.classList.add("React");
+    divReact.style.order = order;
+    divReact.innerText = "😃";
+    return { divDelete, divReact };
 }
 
 function createReactionContainer() {
